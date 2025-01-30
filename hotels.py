af70f6688953fd8eda12f9ef4d9efa814d7f3b0d
Mark Ten <markten2002@gmail.com>
2025-01-30 16:49:21 +0300

    upgrade: add input data examples for FastAPI

--- hotels.py
+++ hotels.py
@@ -1,4 +1,4 @@
-from fastapi import Query, APIRouter
+from fastapi import Query, APIRouter, Body
 
 from schemas.hotels import Hotel, HotelPATCH
 
@@ -42,7 +42,16 @@ def get_hotels(
         summary="Добавление отеля",
         description="Тут можно добавить новый отель",
 )
-def create_hotel(hotel_data: Hotel):  # Использование в качестве параметров атрибуты из класса Hotel
+def create_hotel(hotel_data: Hotel = Body(openapi_examples={  # # Использование в качестве параметров атрибуты из класса Hotel
+    "1": {"summary": "Сочи", "value": {
+        "title": "Отель Сочи 5 звезд у моря",
+        "name": "sochi_u_morya",
+    }}, "2": {"summary": "Дубай", "value": {
+        "title": "Отель Дубай у фонтана",
+        "name": "dubai_fountain",
+    }}, 
+})
+):  
     global hotels
     hotels.append({
         "id": hotels[-1]["id"] + 1,
