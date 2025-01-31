1d4c048cf66a027f9d057b19ebc99a7fff4b726b
Mark Ten <markten2002@gmail.com>
2025-01-31 18:22:11 +0300

    fix repository: change the project structure (sorting into new folders)

--- schemas/hotels.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, Field
-
-class Hotel(BaseModel):  # Класс Hotel для удобного использования данных в коде (принцип dry). BaseModel - класс библиотеки Pydantic
-    title: str
-    name: str
-
-class HotelPATCH(BaseModel): 
-    title: str | None = Field(None)
-    name: str | None = Field(None)
