00b5d22f20f266532300e63b3d083f239b895571
Mark Ten <markten2002@gmail.com>
2025-02-01 22:35:30 +0300

    fix: delete hotels[] we used earlier

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -9,22 +9,8 @@ from src.database import async_session_maker
 from src.schemas.hotels import Hotel, HotelPATCH
 
 
-
-
 router = APIRouter(prefix="/hotels", tags=["Отели"])  # Концепция роутер для подключения ручек hotels к приложению
 
-
-
-
-hotels = [
-    {"id": 1, "title": "Sochi", "name": "sochi"},
-    {"id": 2, "title": "Дубай", "name": "dubai"},
-    {"id": 3, "title": "Мальдивы", "name": "maldivi"},
-    {"id": 4, "title": "Геленджик", "name": "gelendzhik"},
-    {"id": 5, "title": "Москва", "name": "moscow"},
-    {"id": 6, "title": "Казань", "name": "kazan"},
-    {"id": 7, "title": "Санкт-Петербург", "name": "spb"},
-]
 
 '''Создание второй ручки hotels на получение отелей'''
 @router.get(
