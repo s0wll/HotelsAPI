09db406433c8845ffe3d3a548c1f982439f641e6
Mark Ten <markten2002@gmail.com>
2025-02-01 14:54:02 +0300

    feat: add a hotel model for the database

--- src/database.py
+++ src/database.py
@@ -1,7 +1,9 @@
 '''Основной файл для подключения базы данных'''
-'''ORM - общее название для фреймворков или библиотек, позволяющих автоматически связать базу данных с кодом'''
+'''ORM - общее название для фреймворков или библиотек, позволяющих автоматически связать базу данных с кодом,'''
+'''чтобы программист не заморачивался с таблицами и тд, а работал с БД через код'''
 
-from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine  # 1 - , 2 - Асинхронный движок
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine  # 1 - класс для создания сессии (отправление транзакции), 2 - Асинхронный движок
+from sqlalchemy.orm import DeclarativeBase  # Библиотека алхимии ОРМ (для работы с БД через код)
 
 from src.config import settings
 
@@ -10,5 +12,6 @@ engine = create_async_engine(settings.DB_URL)
 
 async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)  # Для создания сессии (отправление транзакции).
                                                                                # Объект сессии (которые мы будем создавать) - своего рода транзакция в базу данных
-session = async_session_maker()
-await session.execute()  # Функция для исполнения запросов
+
+class Base(DeclarativeBase):  # Создаем класс, который ничего не делает. Он нужен, чтобы мы наследовали от него все модели в проекте
+    pass
