2f0ef97133d03d4824750f9c303d38d21960b1e6
Mark Ten <markten2002@gmail.com>
2025-02-04 20:50:55 +0300

    refactor hotels patch endpoint

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -83,16 +83,13 @@ async def edit_hotel(
         summary="Частичное обновление данных об отеле",
         description="Тут мы частично обновляем данные об отеле: можно отправить name, а можно title",
 )
-def partially_edit_hotel(
+async def partially_edit_hotel(
     hotel_id: int,
     hotel_data: HotelPATCH,
 ):
-    global hotels
-    hotel = [hotel for hotel in hotels if hotel["id"] == hotel_id][0]
-    if hotel_data.title:
-        hotel["title"] = hotel_data.title
-    if hotel_data.name:
-        hotel["name"] = hotel_data.name
+    async with async_session_maker() as session:
+        await HotelsRepository(session).edit(hotel_data, exclude_unset=True, id=hotel_id)
+        await session.commit()
     return {"status": "OK"}
 
 
