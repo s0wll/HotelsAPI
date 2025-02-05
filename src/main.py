23d9a7272acb853260d276f6b8dd3f16f7fb515c
Mark Ten <markten2002@gmail.com>
2025-02-05 20:37:54 +0300

    add bcrypt library for use hash, add hashed_password realisation to the /auth post endpoint and add api/auth.py to the main.py

--- src/main.py
+++ src/main.py
@@ -7,10 +7,12 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
 
 from src.api.hotels import router as router_hotels  # Импорт роутера hotels
+from src.api.auth import router as router_auth  # Импорт роутера auth
 
 
 app = FastAPI()  # Приложение - объект класса FastAPI
 
+app.include_router(router_auth)
 app.include_router(router_hotels)  # Подключение роутера hotels к приложению
 
 
