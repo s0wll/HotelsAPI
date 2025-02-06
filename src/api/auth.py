62c33c0ac352e750729162e66f378dc8db32f139
Mark Ten <markten2002@gmail.com>
2025-02-06 13:47:56 +0300

    add jwt token and /login endpoint for auth.py

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,8 +1,10 @@
 # API ручки для /users(endpoints)
-from fastapi import APIRouter
+from datetime import datetime, timezone, timedelta
+
+from fastapi import APIRouter, HTTPException
 
 from passlib.context import CryptContext
-
+import jwt
 
 from src.repositories.users import UsersRepository
 from src.database import async_session_maker
@@ -12,6 +14,16 @@ router = APIRouter(prefix="/auth", tags=["Авторизация и аутент
                    
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+SECRET_KEY = "89d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
 
 @router.post("/register")
@@ -25,3 +37,16 @@ async def register_user(
         await session.commit()
 
     return {"status": "OK"}
+
+
+@router.post("/login")
+async def login_user(
+    data: UserRequestAdd,
+):
+    async with async_session_maker() as session: 
+        user = await UsersRepository(session).get_one_or_none(email=data.email)
+        if not user:
+            raise HTTPException(status_code=401, detail="Пользватель с таким email не зарегестрирован")
+        access_token = create_access_token({"user_id": user.id})
+        return {"access_token": access_token}
+    
