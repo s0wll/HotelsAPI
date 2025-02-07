9750af56721a7aabac27ad9d8e4cb44fb394bdbf
Mark Ten <markten2002@gmail.com>
2025-02-07 18:08:05 +0300

    feat: add bookings model and make migration to db

---
+++ src/migrations/versions/2025_02_07_1804-a903031debd2_add_bookings.py
@@ -0,0 +1,43 @@
+"""add bookings
+
+Revision ID: a903031debd2
+Revises: fbbdcd0d91f1
+Create Date: 2025-02-07 18:04:37.083106
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision: str = "a903031debd2"
+down_revision: Union[str, None] = "fbbdcd0d91f1"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "bookings",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("user_id", sa.Integer(), nullable=False),
+        sa.Column("room_id", sa.Integer(), nullable=False),
+        sa.Column("date_from", sa.Date(), nullable=False),
+        sa.Column("date_to", sa.Date(), nullable=False),
+        sa.Column("price", sa.Integer(), nullable=False),
+        sa.ForeignKeyConstraint(
+            ["room_id"],
+            ["rooms.id"],
+        ),
+        sa.ForeignKeyConstraint(
+            ["user_id"],
+            ["users.id"],
+        ),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("bookings")
