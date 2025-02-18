789683dfcdb6e4109651f470ab8f036b73e620a2
Mark Ten <markten2002@gmail.com>
2025-02-18 13:14:41 +0300

    feat: make realisation of FastAPI task (not Celery)

--- src/api/images.py
+++ src/api/images.py
@@ -1,16 +1,24 @@
 import shutil
 
-from fastapi import APIRouter, UploadFile
+from fastapi import APIRouter, BackgroundTasks, UploadFile
 
 from src.tasks.tasks import resize_image
 
 
 router = APIRouter(prefix="/images", tags=["Изображения"])
 
-@router.post("")
-def upload_image(file: UploadFile):
+# @router.post("")  # Ручка с таской Celery
+# def upload_image(file: UploadFile):
+#     image_path = f"src/static/images/{file.filename}"
+#     with open(f"src/static/images/{file.filename}", "wb+") as new_file:
+#         shutil.copyfileobj(file.file, new_file)
+
+#     resize_image.delay(image_path)
+
+@router.post("")  # Ручка с таской без Celery, напрямую через FastAPI
+def upload_image(file: UploadFile, background_tasks: BackgroundTasks):
     image_path = f"src/static/images/{file.filename}"
     with open(f"src/static/images/{file.filename}", "wb+") as new_file:
         shutil.copyfileobj(file.file, new_file)
 
-    resize_image.delay(image_path)
+    background_tasks.add_task(resize_image, image_path)
