9cea37fbae0ceef5d63f29ef880bfc6a324ea112
Mark Ten <markten2002@gmail.com>
2025-02-20 13:34:46 +0300

    feat: add hotels and rooms test data to the test db (my answer to the task from course)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import json
 import pytest
 from httpx import AsyncClient
 
@@ -5,6 +6,10 @@ from src.main import app
 from src.database import Base, engine_null_pool
 from src.models import *
 from src.config import settings
+from src.utils.db_manager import DBManager
+from src.database import async_session_maker_null_pool
+from src.schemas.hotels import HotelAdd
+from schemas.rooms import RoomAdd
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -19,6 +24,22 @@ async def setup_database(check_test_mode):
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
 
+    with open("tests/mock_hotels.json") as file:
+        hotels_data = json.load(file)
+        for hotel in hotels_data:
+            hotel_data = HotelAdd(**hotel)
+            async with DBManager(session_factory=async_session_maker_null_pool) as db:
+                await db.hotels.add(hotel_data)
+                await db.commit()
+
+    with open("tests/mock_rooms.json") as file:
+        rooms_data = json.load(file)
+        for room in rooms_data:
+            room_data = RoomAdd(**room)
+            async with DBManager(session_factory=async_session_maker_null_pool) as db:
+                await db.rooms.add(room_data)
+                await db.commit()
+    
 
 @pytest.fixture(scope="session", autouse=True)
 async def register_user(setup_database):
