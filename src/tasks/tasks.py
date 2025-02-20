c4f9e28b7992aea61042200a7be745128f4cd44a
Mark Ten <markten2002@gmail.com>
2025-02-20 18:46:07 +0300

    fix: fix some bugs after db engine logic refactored

--- src/tasks/tasks.py
+++ src/tasks/tasks.py
@@ -4,7 +4,7 @@ import os
 
 from PIL import Image
 
-from src.database import async_session_maker_null_pool
+from src.database import async_session_maker
 from src.tasks.celery_app import celery_instance
 from src.utils.db_manager import DBManager
 
@@ -39,7 +39,7 @@ def resize_image(image_path: str):
 
 async def get_bookings_with_today_checkin_helper():
     print("я запускаюсь")
-    async with DBManager(session_factory=async_session_maker_null_pool) as db:
+    async with DBManager(session_factory=async_session_maker) as db:
         bookings = await db.bookings.get_bookings_with_today_checkin()
         print(f"{bookings}")
 
