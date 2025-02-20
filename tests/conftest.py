037f134462c129e6424ecab866a17aa93f349d16
Mark Ten <markten2002@gmail.com>
2025-02-20 12:38:41 +0300

    feat: add register_user test and fix some bugs (httpx version)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,32 @@
 import pytest
+from httpx import AsyncClient
 
+from src.main import app
 from src.database import Base, engine_null_pool
 from src.models import *
 from src.config import settings
 
 
 @pytest.fixture(scope="session", autouse=True)
-async def check_test_mode():
+def check_test_mode():
     assert settings.MODE == "TEST"
     assert settings.DB_NAME == "test"
 
 
 @pytest.fixture(scope="session", autouse=True)
-async def async_main(check_test_mode):
+async def setup_database(check_test_mode):
     async with engine_null_pool.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def register_user(setup_database):
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        await ac.post(
+            "/auth/register",
+            json={
+                "email": "mark@gmail.com",
+                "password": "12345"
+            }
+        )
