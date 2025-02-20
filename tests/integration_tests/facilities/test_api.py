09e597574b5d31d91827c5eac929333377c472e2
Mark Ten <markten2002@gmail.com>
2025-02-20 20:37:45 +0300

    fix: add assert

--- tests/integration_tests/facilities/test_api.py
+++ tests/integration_tests/facilities/test_api.py
@@ -9,5 +9,6 @@ async def test_post_facility(ac):
     assert response.status_code == 200
     res = response.json()
     assert isinstance(res, dict)
+    assert res["status"] == "OK"
     assert res["data"]["title"] == facility_title
     assert "data" in res
