c0e4bbd70e6e2ffddceb153dd1464e2ef5b45181
Mark Ten <markten2002@gmail.com>
2025-03-01 14:44:30 +0300

    fix: fix bug at services/rooms.py

--- src/services/rooms.py
+++ src/services/rooms.py
@@ -37,7 +37,8 @@ class RoomsService(BaseService):
         rooms_facilities_data = [
             RoomFacilityAdd(room_id=room.id, facility_id=f_id) for f_id in room_data.facilities_ids
         ]
-        await self.db.rooms_facilities.add_bulk(rooms_facilities_data)
+        if rooms_facilities_data:
+            await self.db.rooms_facilities.add_bulk(rooms_facilities_data)
         await self.db.commit()
         return room
     
